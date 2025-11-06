aa78444ee11ec22b7336200d00a3872c6453647a
Dmitry Kryushnikov <civ02@mail.ru>
2025-11-06 19:54:21 +0500

    цвета

--- app/src/main/java/ru/kryu/weathertest/ui/theme/Color.kt
+++ app/src/main/java/ru/kryu/weathertest/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package ru.kryu.weathertest.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light theme colors
+val LightBlue80 = Color(0xFFB3D9FF)
+val SkyBlue80 = Color(0xFFCAE5FF)
+val CloudGrey80 = Color(0xFFE0E8F0)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark theme colors
+val DeepBlue40 = Color(0xFF1A5F9E)
+val NightBlue40 = Color(0xFF2C5F8D)
+val StormGrey40 = Color(0xFF536878)
+
+// Additional colors for weather conditions
+val SunnyYellow = Color(0xFFFDB813)
+val RainyBlue = Color(0xFF5A8DC8)
+val CloudyGrey = Color(0xFF9E9E9E)
