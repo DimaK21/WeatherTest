9233a19dae05fa6373dec1f39d888575424f3f93
Dmitry Kryushnikov <civ02@mail.ru>
2025-11-06 21:24:44 +0500

    свайп вниз для обновления

--- app/src/main/java/ru/kryu/weathertest/presentation/screen/weather/WeatherScreen.kt
+++ app/src/main/java/ru/kryu/weathertest/presentation/screen/weather/WeatherScreen.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -24,6 +26,7 @@ import ru.kryu.weathertest.presentation.components.ErrorDialog
 import ru.kryu.weathertest.presentation.components.HourlyForecastRow
 import ru.kryu.weathertest.presentation.components.LoadingIndicator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WeatherScreen(
     viewModel: WeatherViewModel = koinViewModel(),
@@ -43,12 +46,18 @@ fun WeatherScreen(
             }
 
             is WeatherUiState.Success -> {
-                WeatherContent(
-                    state = state,
+                PullToRefreshBox(
+                    isRefreshing = false,
+                    onRefresh = { viewModel.loadWeather() },
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(paddingValues)
-                )
+                ) {
+                    WeatherContent(
+                        state = state,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
             }
 
             is WeatherUiState.Error -> {
